7653edf282e0558b58cd3a77c4316cb1bc6fa34e
joeycast <joey.castagnaro@me.com>
2026-01-04 09:00:03 -0600

    Initial Commit

---
+++ Dandelion/DandelionApp.swift
@@ -0,0 +1,17 @@
+//
+//  DandelionApp.swift
+//  Dandelion
+//
+//  Created by Joe Castagnaro on 1/4/26.
+//
+
+import SwiftUI
+
+@main
+struct DandelionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
